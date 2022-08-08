1f7d7d5cfa8eb1bddae36d78919fe19fdc158133
JH <tnptt1122@gmail.com>
2022-08-09 03:07:20 +0900

    메인화면 디테일 작업

--- lib/screens/home/components/category.dart
+++ lib/screens/home/components/category.dart
@@ -6,58 +6,77 @@ List<Widget> imgList = [
   SvgPicture.asset('assets/icons/category_event.svg', width: 35, height: 35),
   SvgPicture.asset('assets/icons/category_talent.svg', width: 35, height: 35),
   SvgPicture.asset('assets/icons/category_drawing.svg', width: 35, height: 35),
-  SvgPicture.asset('assets/icons/category_home_appliance.svg', width: 35, height: 35),
-  SvgPicture.asset('assets/icons/category_handcraft.svg', width: 35, height: 35),
-  SvgPicture.asset('assets/icons/category_travel_fashion.svg', width: 35, height: 35),
-  SvgPicture.asset('assets/icons/category_seasonal_fashion.svg', width: 35, height: 35)
+  SvgPicture.asset('assets/icons/category_home_appliance.svg',
+      width: 35, height: 35),
+  SvgPicture.asset('assets/icons/category_handcraft.svg',
+      width: 35, height: 35),
+  SvgPicture.asset('assets/icons/category_travel_fashion.svg',
+      width: 35, height: 35),
+  SvgPicture.asset('assets/icons/category_seasonal_fashion.svg',
+      width: 35, height: 35)
 ];
 
-List<String> categories = ['패션', '이벤트', '재능', '그림', '가전제품', '수공예', '여행패션', '계절패션'];
+List<String> categories = [
+  '패션',
+  '이벤트',
+  '재능',
+  '그림',
+  '가전제품',
+  '수공예',
+  '여행패션',
+  '계절패션'
+];
 
 class Category extends StatelessWidget {
   const Category({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            child: Row(
-          children: [Text('카테고리별로 보기'), Text('더보기')],
-        )),
-        Table(
+    return Container(
+        padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
+        child: Column(
           children: [
-            TableRow(children: [
-              Column(
-                children: [imgList[0], Text(categories[0])],
-              ),
-              Column(
-                children: [imgList[1], Text(categories[1])],
-              ),
-              Column(
-                children: [imgList[2], Text(categories[2])],
-              ),
-              Column(
-                children: [imgList[3], Text(categories[3])],
-              )
-            ]),
-            TableRow(children: [
-              Column(
-                children: [imgList[4], Text(categories[4])],
-              ),
-              Column(
-                children: [imgList[4], Text(categories[5])],
-              ),
-              Column(
-                children: [imgList[4], Text(categories[6])],
-              ),
-              Column(
-                children: [imgList[4], Text(categories[7])],
-              )
-            ])
+            Container(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Row(
+                  children: const [
+                    Expanded(flex: 6, child: Text('카테고리별로 보기', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),)),
+                    Expanded(flex: 1, child: Text('더보기'))
+                  ],
+                )),
+            Table(
+              children: [
+                TableRow(children: [
+                  Column(
+                    children: [imgList[0], Text(categories[0])],
+                  ),
+                  Column(
+                    children: [imgList[1], Text(categories[1])],
+                  ),
+                  Column(
+                    children: [imgList[2], Text(categories[2])],
+                  ),
+                  Column(
+                    children: [imgList[3], Text(categories[3])],
+                  )
+                ]),
+                TableRow(children: [
+                  Column(
+                    children: [imgList[4], Text(categories[4])],
+                  ),
+                  Column(
+                    children: [imgList[4], Text(categories[5])],
+                  ),
+                  Column(
+                    children: [imgList[4], Text(categories[6])],
+                  ),
+                  Column(
+                    children: [imgList[4], Text(categories[7])],
+                  )
+                ])
+              ],
+            )
           ],
-        )
-      ],
-    );
+        ));
   }
 }
--- lib/screens/home/components/neighborhood.dart
+++ lib/screens/home/components/neighborhood.dart
@@ -1,19 +1,18 @@
-import 'package:fling/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 List<Widget> imgList1 = [
-  SvgPicture.asset('assets/images/section_1.svg'),
-  SvgPicture.asset('assets/images/section_2.svg'),
-  SvgPicture.asset('assets/images/section_3.svg'),
-  SvgPicture.asset('assets/images/section_4.svg')
+  SvgPicture.asset('assets/images/section_1.svg', width: 350, height: 200),
+  SvgPicture.asset('assets/images/section_2.svg', width: 350, height: 200),
+  SvgPicture.asset('assets/images/section_3.svg', width: 350, height: 200),
+  SvgPicture.asset('assets/images/section_4.svg', width: 350, height: 200)
 ];
 List<Widget> imgList2 = [
-  SvgPicture.asset('assets/icons/circle.svg'),
-  SvgPicture.asset('assets/icons/circle.svg'),
-  SvgPicture.asset('assets/icons/circle.svg'),
-  SvgPicture.asset('assets/icons/circle.svg')
+  SvgPicture.asset('assets/icons/circle.svg', width: 30, height: 30),
+  SvgPicture.asset('assets/icons/circle.svg', width: 30, height: 30),
+  SvgPicture.asset('assets/icons/circle.svg', width: 30, height: 30),
+  SvgPicture.asset('assets/icons/circle.svg', width: 30, height: 30)
 ];
 
 List<String> titleList1 = ['옷 같이 팔아요', '패션 공유해요', '재능 같이 팔아요', '재능 같이 팔아요2'];
@@ -24,21 +23,43 @@ List<String> titleList2 = [
   '트랜드를 앞서 가실 분 모셔요'
 ];
 
-Column Function(int index) frame = (index) {
-  return Column(children: [
-    Expanded(flex: 3, child: imgList1[index]),
-    Expanded(
-        flex: 1,
-        child: Row(children: [
-          Expanded(
-              flex: 2,
-              child: Column(children: [
-                Container(child: Text(titleList1[index])),
-                Container(child: Text(titleList2[index]))
-              ])),
-          Expanded(flex: 1, child: imgList2[index])
-        ]))
-  ]);
+Widget Function(int index) frame = (index) {
+  return Container(
+      margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      decoration: BoxDecoration(
+          color: const Color(0xFFFFFFFF),
+          border: Border.all(color: const Color(0xFFBBBBBB), width: 0.8)),
+      child: Column(children: [
+        Container(child: imgList1[index]),
+        Container(
+            padding: const EdgeInsets.all(15),
+            child: Row(children: [
+              Expanded(
+                  flex: 2,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                            padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                            child: Text(titleList1[index],
+                                style: TextStyle(fontSize: 22))),
+                        Container(
+                            padding: const EdgeInsets.fromLTRB(5, 5, 0, 0),
+                            child: Text(titleList2[index]))
+                      ])),
+              Expanded(
+                  flex: 1,
+                  child: Stack(
+                    children: [
+                      Container(child: imgList2[index]),
+                      Positioned(
+                          left: 25, child: Container(child: imgList2[index])),
+                      Positioned(
+                          left: 50, child: Container(child: imgList2[index]))
+                    ],
+                  ))
+            ]))
+      ]));
 };
 
 class Neighborhood extends StatelessWidget {
@@ -49,25 +70,29 @@ class Neighborhood extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-          child: Text(
-            '우리동네 플리마켓',
-            style: Theme.of(context)
-                .textTheme
-                .headline5
-                ?.copyWith(fontWeight: FontWeight.bold),
-          ),
-        ),
+        Container(
+            padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+            child: Row(
+              children: const [
+                Expanded(
+                    flex: 6,
+                    child: Text(
+                      '우리동네 플리마켓',
+                      style:
+                          TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    )),
+                Expanded(flex: 1, child: Text('더보기'))
+              ],
+            )),
         Container(
             color: const Color(0xFFEDF0F4),
-            height: 280,
+            height: 300,
             child: Padding(
-                padding: const EdgeInsets.fromLTRB(5, 5, 5, 10),
+                padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
                 child: Swiper(
                     autoplay: true,
-                    scale: 0.9,
-                    viewportFraction: 0.8,
+                    scale: 1,
+                    viewportFraction: 1,
                     itemCount: imgList1.length,
                     itemBuilder: (BuildContext context, int index) {
                       return frame(index);
